58bf38329ae2fe4a2956894cac42038bf1d6eb7f
Chad A. Carino <chadc@tekserve.com>
2015-03-03 17:18:08 -0500

    Add search redirect that used to be in separate plugin that is now deprecated.

--- tekserve_theme_widgets.php
+++ tekserve_theme_widgets.php
@@ -19,6 +19,21 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+add_filter('pre_get_posts','tekserve_divert');
+
+
+if ( !function_exists('tekserve_divert') ) {
+	function tekserve_divert($query) {
+		
+		if ( $query->is_search ) {
+			$store_url_pre = 'http://shop.tekserve.com/catalogsearch/result/?q=';
+			$store_url = $store_url_pre . urlencode( $_GET['s'] );
+			wp_redirect($store_url);
+			exit;
+		}
+		return $query;
+	}
+}
 
 
 
